363e505b84367420b5d25c9250f94c8955589952
pkasprzak9 <pawkasprzak9@gmail.com>
2024-01-21 22:21:27 +0100

    Update port configuration for Heroku

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(helmet());

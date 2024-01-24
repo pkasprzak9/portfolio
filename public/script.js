c2122f8d8c3e7a6eefa5340fd8a9d5f5fb39900d
pkasprzak9 <pawkasprzak9@gmail.com>
2024-01-24 22:29:31 +0100

    Refactor script.js and add comments for better readability.

--- public/script.js
+++ public/script.js
@@ -1,26 +1,34 @@
+// Listening for the 'DOMContentLoaded' event to ensure the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
+  // Selecting the scroll indicator element
   const swipeIndicator = document.querySelector('.scroll-indicator');
+  // Selecting the header element
   const header = document.querySelector('.header');
 
+  // Creating an IntersectionObserver to watch visibility changes of the header
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
+      // Toggle the 'fade-out' class based on header visibility
       if (entry.isIntersecting) {
         swipeIndicator.classList.remove('fade-out');
       } else {
         swipeIndicator.classList.add('fade-out');
       }
     });
-  }, { threshold: 0.99 }); // Możemy spróbować z wartością bliską 1, np. 0.99
+  }, { threshold: 0.99 }); // Setting a threshold close to 1 for precise detection
 
+  // Starting to observe the header element
   observer.observe(header);
 });
 
-// Obsługa płynnego przewijania navbara dla starszych przeglądarek
+// Second 'DOMContentLoaded' event listener for smooth scroll functionality
 document.addEventListener('DOMContentLoaded', () => {
+  // Selecting all navigation links that link to an ID on the page
   document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+      e.preventDefault(); // Preventing the default anchor behavior
 
+      // Smooth scrolling to the section linked in the href attribute of the anchor
       document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
       });
@@ -28,12 +36,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// Adding an event listener for the contact form submission
 document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
+  e.preventDefault(); // Preventing the default form submission behavior
 
+  // Extracting email and message values from the form
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
 
+  // Sending a POST request to the '/send-email' endpoint
   fetch('/send-email', {
     method: 'POST',
     headers: {
@@ -43,9 +54,11 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   })
   .then(response => response.text())
   .then(data => {
+    // Alerting the user on successful message send
     alert('Wiadomość wysłana!');
   })
   .catch((error) => {
+    // Handling errors and alerting the user
     console.error('Błąd:', error);
     alert('Błąd wysyłania wiadomości');
   });
